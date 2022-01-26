d63d03a523072babc1447bc83254acf6fa53b6e4
Kenneth MacLean <kenneth.maclean84@gmail.com>
2022-01-26 10:03:45 -0500

    recognize correct and incorrect answers, add or subtract time based on answer, log score, prompt for initials

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,53 +12,81 @@ let question5 = document.querySelector("#question-5");
 let scoreboard = document.querySelector("#scoreboard");
 let splash = document.querySelector(".splash-screen");
 let answers = document.querySelector(".answers");
+// let userName = document.querySelector("#user-name");
+let initials = document.querySelector("#initials")
 
 let quizContent = [ 
     {
-    question: "What is a window object?",
+    question: "Which of the following is NOT a primitive type?",
     choices: [ 
-        "My window that I open to get fresh air",
-        "My operating system",
-        "My Internet Explorer window",
-        "Where my document object is placed"
+        "Number",
+        "String",
+        "Hooligan",
+        "Symbol"
         ],
-    answer: 3
+        answer: "Hooligan"
     },
     {
-        question: "What is a window object?",
+        question: "What does DOM stand for?",
         choices: [ 
-            "My window that I open to get fresh air",
-            "My operating system",
-            "My Internet Explorer window",
-            "Where my document object is placed"
+            "Dope Optics Man",
+            "Document Object Model",
+            "Debut Objection Mode",
+            "Double Operation Model"
             ],
-        answer: 3
-        }
+        answer: "Document Object Model"
+        },
+        {
+            question: "What method attaches an event handler to a specified element?",
+            choices: [ 
+                "innerHTML",
+                "eventHorizon",
+                "getElementById",
+                "addEventListener"
+                ],
+            answer: "addEventListener"
+            },
+            {
+                question: "What is the difference between 'getElementById' and 'querySelector'?",
+                choices: [ 
+                    "'getElementById' takes longer to return",
+                    "'querySelector' can grab either class or id",
+                    "'getElementById' is not Javascript",
+                    "'querySelector' only selects questions"
+                    ],
+                answer: "'querySelector' can grab either class or id"
+                },
+                {
+                    question: "What stops the browser from doing what is would naturally do?",
+                    choices: [ 
+                        "event.preventDefault()",
+                        "event.stopItBrowser()",
+                        "event.nurture()",
+                        "event.distractBrowser()"
+                        ],
+                    answer: "event.preventDefault()"
+                    }
     ]
 
-
-var secondsLeft = 60;
+let secondsLeft = 60;
 
 playButton.addEventListener('click', setTime);
     function setTime() {
-        // Sets interval in variable
         var timerInterval = setInterval(function() {
           secondsLeft--;
           timer.textContent = secondsLeft;
       
-          if(secondsLeft === 0) {
-            // Stops execution of action at set interval
+          if(secondsLeft <= 0) {
             clearInterval(timerInterval);
-            // Calls function to create and append image
-            sendMessage();
           }
       
         }, 1000);
+
     hide(splash)
     show(questionContainer)
     displayQuiz()
     }
-
+    //hide element
     function hide(element) {
         element.setAttribute("style", "display: none;");
     }
@@ -67,22 +95,64 @@ playButton.addEventListener('click', setTime);
         element.setAttribute("style", "display: block;");
     }
     let quizIndex=0
-
-    function displayQuiz() {
+    //display question and listen for answer
+    function displayQuiz(event) {
         currentQuestion=quizContent[quizIndex];
         questionEl.textContent=currentQuestion.question;
-        
+
         let currentAnswers=currentQuestion.choices;
         for (let i=0; i < currentAnswers.length; i++){
             answers.children[i].textContent=currentAnswers[i];
-            console.log(currentAnswers)
-            console.log(quizContent.choices)
+            answers.children[i].addEventListener('click', nextQuestion);
         }
+    
+    }
+    function nextQuestion(event){ 
+        if (event.target.textContent === quizContent[quizIndex].answer){
+            secondsLeft+=5;
+        } else {
+            secondsLeft-=10;
+        }
+        //recognize answers using event.target
+        //give score, decrease time if wrong
+
+        if (quizIndex < quizContent.length-1){ 
+            quizIndex++; 
+            displayQuiz();
+        } else {
+            endQuiz();
+        }
+    }
+
+    // let  = localStorage.getItem("score");
+
+    //add event listener to become legend button
+    //make separate function for grabbing and saving initials after endquiz before scorebaord
+    function saveUserName(){
+        let userName = document.querySelector("#user-name");
+
+        localStorage.setItem("userName", userName.textContent);
+    }
+
+    function endQuiz(){
+        let userName = document.querySelector("#user-name");
+        console.log(userName)
+        hide(questionContainer);
+        score.textContent = secondsLeft;
+        show(initials);
+        initials.addEventListener('click', saveUserName);
+        localStorage.setItem("score", score.textContent);
+        localStorage.getItem("score");
+        localStorage.setItem("userName", userName.textContent);
+        console.log(userName.textContent)
+        // displayScoreboard();
 
     }
 
+    // function displayScoreboard(){
+        // JSONparse.getItem(key)
 
-
+    // }
 //   highscoreButton.addEventListener('click', function() {
 //     score;
 //   });
